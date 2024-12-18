68a1e072f30624185a2ce868143b8e63d9aff686
FonsahPageo <ashprincepageo@gmail.com>
2024-12-18 20:28:45 +0100

    complete project

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,36 @@
-import { StrictMode } from "react";
+import { StrictMode, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-// import App from "./App.jsx";
 import { CreateCv } from "./components/form.jsx";
 import { FinalCV } from "./components/cv.jsx";
 
+
+export function CvApp() {
+  const [cvData, setCvData] = useState({
+    personalDetails: {
+      fullName: "",
+      jobTitle: "",
+      email: "",
+      phoneNumber: "",
+      address: "",
+    },
+    profile: "",
+    education: [],
+    experience: [],
+  });
+
+  return (
+    <div className="container">
+      <CreateCv cvData={cvData} setCvData={setCvData} />
+      <FinalCV cvData={cvData} />
+    </div>
+  )
+}
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <App /> */}
-    <CreateCv />
-    <FinalCV />
+    <CvApp />
+    {/* <CreateCv />
+    <FinalCV /> */}
   </StrictMode>
 );
